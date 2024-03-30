25d151f864f4fd4a588f1d04e19e180e4d724ee0
Samuel Carvajal <samuelcarvajalgrau06@gmail.com>
2024-03-29 23:23:03 -0500

    Agregue archivo anagrama

--- pythonProyects/logica/02_anagrama.py
+++ pythonProyects/logica/02_anagrama.py
@@ -6,8 +6,8 @@ def limpiar_consola():
 
 
 def ingresar_palabras():
-    palabra_uno = input("Ingresa un Palabra: ")
-    palabra_dos = input("Ingresa otra palabra: ")
+    palabra_uno = input("Ingresa un Palabra: ").lower()
+    palabra_dos = input("Ingresa otra palabra: ").lower()
     return palabra_uno, palabra_dos
 
 
@@ -15,6 +15,8 @@ def anagrama():
     limpiar_consola()
     palabra_uno, palabra_dos = ingresar_palabras()
 
+    if palabra_uno == palabra_dos:
+        return print("No es un anagrama")
     # La función set() en Python se utiliza para crear un conjunto, que es una colección desordenada de elementos únicos.
     if set(palabra_uno) == set(palabra_dos):
         return print("Es un anagrama")
