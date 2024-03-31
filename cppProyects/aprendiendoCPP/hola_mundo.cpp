61b9d248b53327adab3ffc220f482eb767507381
Samuel Carvajal <samuelcarvajalgrau06@gmail.com>
2024-03-31 18:44:36 -0500

    Hello World

---
+++ cppProyects/aprendiendoCPP/hola_mundo.cpp
@@ -0,0 +1,18 @@
+// Primer programa en C++
+
+// Incluimos la biblioteca iostream para usar la función cout
+#include <iostream>
+
+// Usamos el espacio de nombres std
+using namespace std;
+
+// Función principal (entry point del programa)
+int main()
+{
+
+    // Imprimimos "Hola Mundo" en la consola
+    cout << "Hola Mundo" << endl;
+
+    // Indicamos que el programa termina correctamente
+    return 0;
+}
