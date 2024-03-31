ff05881872010644031c4bd35b2410a05927c06a
Samuel Carvajal <samuelcarvajalgrau06@gmail.com>
2024-03-30 22:15:05 -0500

    New Files

--- pythonProyects/logica/05_area_poligonos.py
+++ pythonProyects/logica/05_area_poligonos.py
@@ -12,12 +12,7 @@ def poligono_usuario():
             "¿A qué poligono le desea calcular el area?\n\n1. Cuadrado\n2. Rectángulo\n3. Triángulo\n\n==> "
         )
     )
-    if poligono != 1:
-        limpiar_consola()
-        print("Poligono Invalido")
-        exit()
-    else:
-        return poligono
+    return poligono
 
 
 def valores_poligono():
