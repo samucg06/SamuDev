d24cf90cfb4950967c7e7069e44ec0e0986b4a91
Samuel Carvajal <samuelcarvajalgrau06@gmail.com>
2024-04-06 22:19:14 -0500

    Include functionality to find cathetus

--- cppProjects/aprendiendoLenguajeCPP/retos/06_teorema_pitagora.cpp
+++ cppProjects/aprendiendoLenguajeCPP/retos/06_teorema_pitagora.cpp
@@ -3,7 +3,33 @@
 
 using namespace std;
 
-float teorema_pitagoras()
+int procedimiento_pitagorico()
+{
+    int procedimiento;
+
+    cout << "Desea encontrar:\n\n1. Hipotenusa\n2. Cateto\n\n==> ";
+    cin >> procedimiento;
+
+    return procedimiento;
+}
+
+float encontrar_cateto()
+{
+    float hipotenusa, cateto_a, cateto_b;
+
+    cout << "Ingresa el valor de la hipotenusa: ";
+    cin >> hipotenusa;
+
+    cout << "Ingresa el valor del cateto dado: ";
+    cin >> cateto_a;
+
+    cateto_b = pow(hipotenusa, 2) - pow(cateto_a, 2);
+    cateto_b = sqrt(cateto_b);
+
+    return cateto_b;
+}
+
+float encontrar_hipotenusa()
 {
     float cateto_a, cateto_b, hipotenusa;
 
@@ -21,10 +47,22 @@ float teorema_pitagoras()
 
 int main()
 {
-    float hipotenusa = teorema_pitagoras();
+    int procedimiento = procedimiento_pitagorico();
 
-    cout.precision(3);
-    cout
-        << "La longitud de la hipotenusa del triangulo rectangulo es: " << hipotenusa;
+    if (procedimiento == 1)
+    {
+        cout.precision(3);
+        float hipotenusa = encontrar_hipotenusa();
+        cout << endl
+             << "El valor de la hipotenusa del triangulo es: " << hipotenusa;
+    }
+    else if (procedimiento == 2)
+    {
+        cout.precision(3);
+        float cateto = encontrar_cateto();
+        cout << endl
+             << "El valor del cateto faltante del triangulo es: " << cateto;
+    }
+
     return 0;
 }
