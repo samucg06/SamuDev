5bb8a259a8161c0c8762615a885438a4285960ca
Samuel Carvajal <samuelcarvajalgrau06@gmail.com>
2024-04-14 18:46:08 -0500

    New File

---
+++ cppProjects/varios/01_notas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+float calcularNotaFinal()
+{
+    return 0;
+}
+
+int main()
+{
+    int eleccionFusion,cantidadNotas;
+
+    cout << "Ingrese la cantidad de Notas";
+    cin >> cantidadNotas;
+
+    return 0;
+}
